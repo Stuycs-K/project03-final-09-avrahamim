70385ab81c45261ca0b5676b9f5e3e2233ace86c
Michael-Av <144476005+Michael-Av@users.noreply.github.com>
2025-01-20 12:12:06 -0500

    Update game_player.h to include function ehaders"

--- game_player.h
+++ game_player.h
@@ -5,6 +5,9 @@
 #include <sys/ipc.h>
 #include <sys/types.h>
 #include <time.h>
+
+#ifndef PLAYER_H
+#define PLAYER_H
 #define VICTORY -10 // Randomly chosen numbers that cannot possibly
 #define LOSS -20 // match any game numbers
 #define TIMELOSS -30 //
@@ -21,3 +24,9 @@ struct itimerspec stop_timer = {
   .it_interval = {.tv_sec = 0, .tv_nsec = 0},
   .it_value = {.tv_sec = 0, .tv_nsec = 0}
 };
+
+void sigalrm_handler(int s);
+int stringToNum(char* string, int size);
+int getPlayerInput();
+
+#endif
